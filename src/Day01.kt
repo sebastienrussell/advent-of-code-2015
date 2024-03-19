80415cc99a336b993aa384555edec002755d6e71
Sebastien Russell <sebastienrussell@users.noreply.github.com>
2024-03-19 08:42:19 -0400

    Add day1
    Signed-off-by: sebastienrussell <russell.sebas@gmail.com>

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var floor = 0
+        input.first().forEach {
+            when (it) {
+                '(' -> floor++
+                ')' -> floor--
+            }
+        }
+        return floor
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var floor = 0
+        var firstBasementPosition = 0
+
+        val line = input.first()
+        var i = 0
+        while (firstBasementPosition == 0) {
+            when (line[i]) {
+                '(' -> floor++
+                ')' -> floor--
+            }
+            if (floor == -1)
+                firstBasementPosition = i
+            else
+                i++
+        }
+        return firstBasementPosition + 1
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     part1(input).println()
